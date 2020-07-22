2ef46446e469c9b905178841a906e2369cf21b0f
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-07-21 21:20:38 -0500

    Drafted sign-in page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,18 +4,19 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import SignInScreen from './screens/SignInScreen';
 
 import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/sign-in' component={SignInScreen} />
       </Layout>
     );
   }
