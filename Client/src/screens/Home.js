4f25916133343cb45cb42033e8d3353d5ca80cb9
Tony Vu <52386267+tonyvu1@users.noreply.github.com>
2020-08-12 16:38:40 -0500

    Got started on homepage layout

--- Client/src/screens/Home.js
+++ Client/src/screens/Home.js
@@ -1,7 +1,12 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Cookies from "universal-cookie";
+import exampleImg from "../assets/example.jpg";
+import Leaderboard from "../components/Leaderboard";
+import Certificates from "../components/Certificates";
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
 
 const Home = () => {
   const jwt = useSelector((state) => state.jwt);
@@ -12,21 +17,214 @@ const Home = () => {
     console.log(cookies.get("jwt"));
   };
 
+  const name = cookies.get("firstName");
+  const points = cookies.get("points");
+
+  const myRef = useRef(null);
+  const executeScroll = () => scrollToRef(myRef);
+
   return (
-    <div className="jumbotron">
-      <h1 className="display-4">Hello, world!</h1>
-      <p className="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <hr className="my-4" />
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
-      </p>
-      <a className="btn btn-primary btn-lg" href="#" role="button">
-        Learn more
-      </a>
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome back, {name}!</h1>
+        <p className="lead">
+          All courses offer a downloadable certificate upon completion. In
+          addition, you can earn points for each course you complete.
+        </p>
+        <hr className="my-4" />
+        <div className="row">
+          <div className="col-md-4">
+            <p className="d-flex justify-content-center">
+              <b>What would you like to do today?</b>
+            </p>
+            <p className="d-flex justify-content-center">
+              <button className="btn btn-primary" onClick={executeScroll}>
+                Take a course
+              </button>
+            </p>
+            <p className="d-flex justify-content-center">
+              <Link to="/profile" style={{ color: "black" }}>
+                <button className="btn btn-primary">View my profile</button>
+              </Link>
+            </p>
+            <p className="d-flex justify-content-center">
+              <Link to="/" style={{ color: "black" }}>
+                Example Certificate 3
+              </Link>
+            </p>
+            <p className="d-flex justify-content-center">
+              <Link to="/" style={{ color: "black" }}>
+                Example Certificate 4
+              </Link>
+            </p>
+            <p className="d-flex justify-content-center">
+              <Link to="/" style={{ color: "black" }}>
+                Example Certificate 5
+              </Link>
+            </p>
+            <p className="d-flex justify-content-center">
+              <Link to="/" style={{ color: "black" }}>
+                View all
+              </Link>
+            </p>
+          </div>
+          <div className="col-md-4">
+            <p className="d-flex justify-content-center">
+              <b>My Points</b>
+            </p>
+            <p
+              className="d-flex justify-content-center"
+              style={{ fontSize: "120px" }}
+            >
+              {points}
+            </p>
+          </div>
+          <div className="col-md-4">
+            <Leaderboard />
+          </div>
+        </div>
+      </div>
+
+      <div className="jumbotron" ref={myRef}>
+        <h1 className="display-4" style={{ fontSize: "36px" }}>
+          Beginner Courses
+        </h1>
+        <hr className="my-4" />
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4" id="courses-top">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="jumbotron">
+        <h1 className="display-4" style={{ fontSize: "36px" }}>
+          Intermediate Courses
+        </h1>
+        <hr className="my-4" />
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="jumbotron">
+        <h1 className="display-4" style={{ fontSize: "36px" }}>
+          Advanced Courses
+        </h1>
+        <hr className="my-4" />
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <img src={exampleImg} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Course 1</h5>
+                <p className="card-text">Course summary will go here</p>
+                <a href="#" className="btn btn-primary">
+                  Take course
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
